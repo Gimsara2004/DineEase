a4d72f22f68340fb0ad2a7c4c59b13cd5854c0b3
Gimsara <gimsarapieris6@gmail.com>
2026-04-22 09:57:50 +0530

    Save booking to database

---
+++ config/reservation.php
@@ -0,0 +1,33 @@
+<?php
+include 'config/db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = $_POST['name'];
+    $phone   = $_POST['phone'];
+    $email   = $_POST['email'];
+    $date    = $_POST['date'];
+    $time    = $_POST['time'];
+    $guests  = $_POST['guests'];
+    $message = $_POST['message'];
+
+    $stmt = $conn->prepare("INSERT INTO reservations 
+        (name, phone, email, date, time, guests, message) 
+        VALUES (?, ?, ?, ?, ?, ?, ?)");
+    
+    $stmt->bind_param("sssssss", 
+        $name, $phone, $email, 
+        $date, $time, $guests, $message);
+    
+    if ($stmt->execute()) {
+        echo json_encode([
+            "status"  => "success",
+            "message" => "Reservation confirmed!"
+        ]);
+    } else {
+        echo json_encode([
+            "status"  => "error",
+            "message" => "Something went wrong."
+        ]);
+    }
+}
+?>
