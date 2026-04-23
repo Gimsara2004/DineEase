1fbb7c097ff1cdf2b5558052477f3b70f01a34e5
Gimsara <gimsarapieris6@gmail.com>
2026-04-23 08:16:54 +0530

    Fix DB name

--- config/db.php
+++ config/db.php
@@ -2,11 +2,16 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "restaurant_db";
+$db   = "dineease_db";   // ← was "restaurant_db" (WRONG)
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("<p style='font-family:sans-serif;color:red;padding:20px'>
+        ❌ <b>Database connection failed.</b><br>
+        Did you run setup.sql in phpMyAdmin?<br><br>
+        Error: " . $conn->connect_error . "
+    </p>");
 }
+$conn->set_charset('utf8mb4');
 ?>
