25f9233b25eab4cf6d8cbf05fa3e26006aa8a7e0
Gimsara <gimsarapieris6@gmail.com>
2026-04-23 10:44:43 +0530

    change from dineease_db to restaurant_db

--- config/db.php
+++ config/db.php
@@ -2,15 +2,13 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "dineease_db";   // ← was "restaurant_db" (WRONG)
+$db   = "restaurant_db";  
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("<p style='font-family:sans-serif;color:red;padding:20px'>
-        ❌ <b>Database connection failed.</b><br>
-        Did you run setup.sql in phpMyAdmin?<br><br>
-        Error: " . $conn->connect_error . "
+    die("<p style='font-family:sans-serif;color:red;padding:30px'>
+        <b>❌ DB Error:</b><br><br>" . $conn->connect_error . "
     </p>");
 }
 $conn->set_charset('utf8mb4');
